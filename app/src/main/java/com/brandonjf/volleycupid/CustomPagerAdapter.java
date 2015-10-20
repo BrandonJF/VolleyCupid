154295a789eeb5262b99897a1818c72e5b73a7b7
Brandon John-Freso <brandon@okcupid.com>
2015-10-20 17:50:33 -0400

    ViewPager in RecyclerView actually working... I don't know how, but mutauvgawsh it is.

---
+++ app/src/main/java/com/brandonjf/volleycupid/CustomPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.brandonjf.volleycupid;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.toolbox.NetworkImageView;
+import com.brandonjf.volleycupid.okclasses.QuickmatchMatch;
+
+/**
+ * Created by brandon on 10/20/15.
+ */
+public class CustomPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private QuickmatchMatch mMatch;
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+    }
+
+    public CustomPagerAdapter(Context context, QuickmatchMatch match) {
+        mContext = context;
+        mMatch = match;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.photo_page, container, false);
+        NetworkImageView pageUserPhoto = (NetworkImageView) layout.findViewById(R.id.page_user_image);
+        pageUserPhoto.setImageUrl(mMatch.getThumbs().get(position).get400x400(), ApplicationController.getInstance().getmImageLoader());
+        container.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object view) {
+        container.removeView((View) view);
+    }
+
+    @Override
+    public int getCount() {
+        return mMatch.getThumbs().size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
--- app/src/main/java/com/brandonjf/volleycupid/OkClasses/OkRecyclerViewAdapter.java
+++ app/src/main/java/com/brandonjf/volleycupid/OkClasses/OkRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.brandonjf.volleycupid.okclasses;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import com.android.volley.toolbox.NetworkImageView;
 import com.brandonjf.volleycupid.ApplicationController;
+import com.brandonjf.volleycupid.CustomPagerAdapter;
 import com.brandonjf.volleycupid.R;
 
 import java.util.ArrayList;
@@ -36,6 +38,7 @@ public class OkRecyclerViewAdapter extends RecyclerView.Adapter<OkRecyclerViewAd
         matchViewHolder.username.setText(match.getUsername());
         matchViewHolder.age.setText(match.getUserinfo().getAge().toString());
         matchViewHolder.userPhoto.setImageUrl(match.getThumbs().get(0).get400x400(), ApplicationController.getInstance().getmImageLoader());
+        matchViewHolder.viewPager.setAdapter(new CustomPagerAdapter(matchViewHolder.viewPager.getContext().getApplicationContext(), match));
     }
 
     @Override
@@ -59,6 +62,7 @@ public class OkRecyclerViewAdapter extends RecyclerView.Adapter<OkRecyclerViewAd
         CardView cardView;
         TextView username, age;
         NetworkImageView userPhoto;
+        ViewPager viewPager;
 
         MatchViewHolder(View matchView) {
             super(matchView);
@@ -66,6 +70,9 @@ public class OkRecyclerViewAdapter extends RecyclerView.Adapter<OkRecyclerViewAd
             username = (TextView) matchView.findViewById(R.id.tv_username);
             age = (TextView) matchView.findViewById(R.id.tv_age);
             userPhoto = (NetworkImageView) matchView.findViewById(R.id.user_image);
+            viewPager = (ViewPager) matchView.findViewById(R.id.viewPager);
+//            viewPager.setAdapter(new CustomPagerAdapter(viewPager.getContext().getApplicationContext()));
+
 
 
         }
