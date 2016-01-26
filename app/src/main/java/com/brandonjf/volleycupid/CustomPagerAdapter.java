7ec420b234cd3c1a087f0d2321345e2587ed48fd
Brandon John-Freso <brandon@okcupid.com>
2016-01-25 20:34:16 -0500

    Trying a new layout

--- app/src/main/java/com/brandonjf/volleycupid/CustomPagerAdapter.java
+++ app/src/main/java/com/brandonjf/volleycupid/CustomPagerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.android.volley.toolbox.NetworkImageView;
 import com.brandonjf.volleycupid.okclasses.QuickmatchMatch;
@@ -31,6 +32,10 @@ public class CustomPagerAdapter extends PagerAdapter {
         LayoutInflater inflater = LayoutInflater.from(mContext);
         ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.photo_page, container, false);
         NetworkImageView pageUserPhoto = (NetworkImageView) layout.findViewById(R.id.page_user_image);
+        TextView username = (TextView) layout.findViewById(R.id.tv_username);
+        TextView information = (TextView) layout.findViewById(R.id.tv_age);
+        username.setText(mMatch.getUsername());
+        information.setText("Age " +  mMatch.getUserinfo().getAge().toString());
         pageUserPhoto.setImageUrl(mMatch.getThumbs().get(position).get400x400(), ApplicationController.getInstance().getmImageLoader());
         container.addView(layout);
         return layout;
